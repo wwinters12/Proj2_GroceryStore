5dd73db2254af6aa7b621f25fd2065ef568c35b6
iyayi0106 <iyayi0106@gmail.com>
2022-04-01 11:50:18 -0400

    Item methods implemented

--- main/Item.java
+++ main/Item.java
@@ -1,5 +1,32 @@
 package main;
 
 public class Item {
+    private String name;
+    private double price;
+    private boolean isTaxed;
+    private int quantity;
+
+    public Item(String nameIn, double priceIn, boolean isTaxedIn, int quantityIn) {
+        this.name = nameIn;
+        this.price = priceIn;
+        this.isTaxed = isTaxedIn;
+        this.quantity = quantityIn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isTaxed() {
+        return isTaxed;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
     
 }
