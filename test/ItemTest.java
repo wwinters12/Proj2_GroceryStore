eef4b7a2d6c48338064db1e765ab351f95eb73fc
iyayi0106 <iyayi0106@gmail.com>
2022-04-01 12:31:34 -0400

    Item unit tests implemented

--- test/ItemTest.java
+++ test/ItemTest.java
@@ -1,5 +1,22 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import main.Item;
+
 public class ItemTest {
+    
+    @Test
+    public void constructorTest(){
+        Item blackberries = new Item("blackberries", 4.99, true, 20);
+
+        assertEquals(blackberries.getName(), "blackberries"); //getName test
+        assertEquals(blackberries.getPrice(), 4.99, 0.00); //getPrice test
+        assertEquals(blackberries.isTaxed(), true); //isTaxed test
+        assertEquals(blackberries.getQuantity(), 20); //getQuantity test
+
+    }
     
 }
