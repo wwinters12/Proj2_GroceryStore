4efe2791d7b52eae4acec0c3ab23460b0a733a38
Jacob Reed <jakereed292@gmail.com>
2022-04-20 11:44:23 -0400

    Removed Abstract User

--- main/user/Customer.java
+++ main/user/Customer.java
@@ -1,16 +1,34 @@
 package main.user;
 
+import java.util.ArrayList;
+
 import main.Cart;
 import main.Item;
 
-public class Customer extends AbstractUser{
+public class Customer{
+    Cart cart;
+    int idNum;
 
-    public Customer(int idNum, Cart cart) {
-        super(idNum, cart);
+    public Customer(int idNumIn, Cart cartIn) {
+        cart = cartIn;
+        idNum = idNumIn;
     }
 
     public int getId(){
         return idNum;
+    }
+
+    public ArrayList<String> printBill(){
+        ArrayList<String> bill = new ArrayList<String>();
+        cart.getCart();
+        if(cart.cart.size() == 0){
+            throw new IllegalArgumentException("Nothing in the cart");
+        }
+        for(int i = 0; i < cart.cart.size(); i++){
+            bill.add("Name: " + cart.cart.get(i).getName() + "\tPrice: " + cart.cart.get(i).getPrice()
+            + "\tQuantity: " + cart.cart.get(i).getQuantity());
+        }
+        return bill;
     }
 
     public void addItem(Item item){
