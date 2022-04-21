f772a7c6248934545450256772f65838cbe48845
Jacob Reed <jakereed292@gmail.com>
2022-04-21 15:17:07 -0400

    Updated Usercart

--- main/user/Customer.java
+++ main/user/Customer.java
@@ -6,11 +6,11 @@ import main.Cart;
 import main.Item;
 
 public class Customer{
-    Cart cart;
+    Cart Usercart;
     int idNum;
 
     public Customer(int idNumIn, Cart cartIn) {
-        cart = cartIn;
+        Usercart = cartIn;
         idNum = idNumIn;
     }
 
@@ -20,13 +20,13 @@ public class Customer{
 
     public ArrayList<String> printBill(){
         ArrayList<String> bill = new ArrayList<String>();
-        cart.getCart();
-        if(cart.cart.size() == 0){
+        Usercart.getCart();
+        if(Usercart.cart.size() == 0){
             throw new IllegalArgumentException("Nothing in the cart");
         }
-        for(int i = 0; i < cart.cart.size(); i++){
-            bill.add("Name: " + cart.cart.get(i).getName() + "\tPrice: " + cart.cart.get(i).getPrice()
-            + "\tQuantity: " + cart.cart.get(i).getQuantity());
+        for(int i = 0; i < Usercart.cart.size(); i++){
+            bill.add("Name: " + Usercart.cart.get(i).getName() + "\tPrice: " + Usercart.cart.get(i).getPrice()
+            + "\tQuantity: " + Usercart.cart.get(i).getQuantity());
         }
         return bill;
     }
