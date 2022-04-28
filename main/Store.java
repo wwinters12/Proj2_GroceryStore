544f8545e55818f2a28bad73c721465addc67869
iyayi0106 <iyayi0106@gmail.com>
2022-04-28 14:40:39 -0400

    initiated Store class/tests

--- main/Store.java
+++ main/Store.java
@@ -1,5 +1,40 @@
 package main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import main.user.Employee;
+
 public class Store {
+    private List<Item> inventory = new ArrayList<>();
+    private List<Employee> employees = new ArrayList<>();
+
+    public Store(){
+        //empty constructor
+    }
+
+    /**
+     * 
+     * @param itemToAdd
+     */
+    public void addItemToInventory(Item itemToAdd){
+
+    }
     
+    /**
+     * 
+     * @param employeeToAdd
+     */
+    public void addEmployee(Employee employeeToAdd){
+
+    }
+
+    public List<Item> getInventory() {
+        return inventory;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
 }
